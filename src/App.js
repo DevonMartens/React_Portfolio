f8144da4f5610d3b8994af825e9f63965df72510
DevonMartens <dmmartens91@gmail.com>
2020-04-23 12:50:57 -0400

    navbar selected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(https://cdn2.atlantamagazine.com/wp-content/uploads/sites/4/2020/03/atlantablue_getty.jpg) center / cover'}}>
+        <Header transparent title="Devon Martens" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a> 
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
   );
 }
 
 export default App;
+ 
---
+++ src/data/info.js
@@ -0,0 +1,9 @@
+const personalInfo = {
+    name: 'Devon Martens',
+    linkedIn: 'https://www.linkedin.com/devon-',
+    github: 'https://github.com/DevonMartens',
+    email: 'mailto:dmmartens91@gmail.com',
+    resume: 'https://docs.google.com/document/d/1ZdoMr1TXAG_keTHIUCTV8WdhTF-bMYp9Q5ReJE5EuKY/edit?usp=sharing'
+  };
+  
+  export { personalInfo };

6c28b69851c0cb3eb2a71839fd5f5d7c6fc639ec
DevonMartens <dmmartens91@gmail.com>
2020-04-23 13:49:18 -0400

    about to run a test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
@@ -25,6 +27,12 @@ function App() {
         </Drawer>
         <Content />
     </Layout>
+    <Content>
+    <Main> </Main>
+    <Main> </Main>
+    </Content>
+  
+ 
 </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css'
 import 'react-mdl/extra/material.js'
+import { BrowserRouter, Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
  
     <App />
+ 
   </React.StrictMode>,
   document.getElementById('root')
 );

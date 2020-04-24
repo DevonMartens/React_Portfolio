4f1758bf299991ef14877fa1036fde401cc19352
DevonMartens <dmmartens91@gmail.com>
2020-04-24 10:54:22 -0400

    odd frame placement for gif and wierd pink links need fixing

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -13,10 +13,18 @@ class aboutMe extends Component {
         <Jumbotron  style={{ background: 'url(https://pix10.agoda.net/geo/city/2045/1_2045_02.jpg?s=1920x822)'}}>
         <Container>
  
-   <div>
+   <div className="row">
+       <div className="col-3" style={{  
+         background: 'url(https://i.pinimg.com/originals/db/03/96/db039626bfa2ecc53f0f6e7f335aef4c.gif)',
+         backgroundRepeat: 'none',
+         }}>
+             <Container>
           <img src={headShot} atl="devon"></img>
+          </Container>
+          </div>
         <h3 className="mt-5">Devon's Coding Abilities</h3> 
         </div> 
+
         <Card>
         <AnimatedTypingComponent></AnimatedTypingComponent>
        

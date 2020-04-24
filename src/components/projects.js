811b1806a4edb81ccb5d196d1fd76b7f290a9401
DevonMartens <dmmartens91@gmail.com>
2020-04-24 13:25:00 -0400

    some photo's selection

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,7 +8,6 @@ class Projects extends Component {
     render() {
     return (
 
-    
           <MDBContainer>
              <hi>There are 8 Project in zt</hi>
             <MDBCarousel
@@ -23,41 +22,42 @@ class Projects extends Component {
                 <MDBView>
                   <img
                     className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                    src="https://img.cinemablend.com/filter:scale/quill/a/4/d/1/e/f/a4d1ef8a005bc5a618fe28d563600b485ba7f473.jpg?fw=1200"
                     alt="First slide"
                   />
                 <MDBMask overlay="black-light" />
                 </MDBView>
                 <MDBCarouselCaption>
-                  <h3 className="h3-responsive">Light mask</h3>
-                  <p>First text</p>
+                  <h3 className="h3-responsive">Friend Finder</h3>
+                  <p>A NodeJS app with Express Servers and Jquery</p>
+                  <p>Find out the answer to who should be your friend. Life's greatest mystery!</p>
                 </MDBCarouselCaption>
               </MDBCarouselItem>
               <MDBCarouselItem itemId="2">
                 <MDBView>
                   <img
                     className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
+                    src="https://steamcdn-a.akamaihd.net/steam/apps/628570/header.jpg?t=1532519256"
                     alt="Second slide"
                   />
                 <MDBMask overlay="black-strong" />
                 </MDBView>
                 <MDBCarouselCaption>
-                  <h3 className="h3-responsive">Strong mask</h3>
-                  <p>Second text</p>
+                  <h3 className="h3-responsive">Jquery Trivia Game</h3>
+                  <p>One of my first apps. Demonstrates my problem soling abilities and Jquery abilities.</p>
                 </MDBCarouselCaption>
               </MDBCarouselItem>
               <MDBCarouselItem itemId="3">
                 <MDBView>
                   <img
                     className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg"
+                    src="https://glhtainplano.files.wordpress.com/2015/06/extra-extra.png"
                     alt="Third slide"
                   />
                 <MDBMask overlay="black-slight" />
                 </MDBView>
                 <MDBCarouselCaption>
-                  <h3 className="h3-responsive">Slight Mast</h3>
+                  <h3 className="h3-responsive" text="danger">Slight Mast</h3>
                   <p>Third text</p>
                 </MDBCarouselCaption>
               </MDBCarouselItem>
